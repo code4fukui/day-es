f54690dc9acde427e996af41093de325f4b61b16
Taisuke Fukuno <fukuno@jig.jp>
2021-08-02 07:53:34 +0900

    add Time class

---
+++ Time.js
@@ -0,0 +1,84 @@
+import { fix0 } from "https://js.sabae.cc/fix0.js";
+
+class Time {
+  constructor(time) {
+    if (time == undefined) {
+      const d = new Date();
+      this.hour = d.getHours();
+      this.min = d.getMinutes();
+      this.sec = d.getMinutes();
+      this.msec = d.getMilliseconds();
+    } else if (typeof time == "string") {
+      //const n = time.match(/(\d+):(\d+)(:(\d+).(\d))/);
+      //const n = time.match(/(\d+):(\d+)(:(\d+))?/);
+      const n = time.match(/(-)?(\d+):(\d+)(:(\d+)(.(\d+))?)?/);
+      //console.log(n);
+      if (n) {
+        this.minus = n[1] == "-";
+        this.hour = parseInt(n[2], 10);
+        this.min = parseInt(n[3], 10);
+        if (n[5] != undefined) {
+          this.sec = parseInt(n[5], 10);
+        }
+        if (n[7] != undefined) {
+          this.msec = parseInt(n[7], 10);
+        }
+      }
+    } else if (typeof time == "number") {
+      this.minus = time < 0;
+      time = Math.abs(time);
+      this.hour = Math.floor(time / (60 * 60));
+      this.min = Math.floor(time / 60) % 60;
+      this.sec = Math.floor(time) % 60;
+      this.msec = Math.floor(time) % 60 == time % 60 ? undefined : Math.floor(time % 1 * 1000);
+    }
+  }
+  toSeconds() {
+    const res = (this.hour || 0) * 60 * 60 + (this.min || 0) * 60 + (this.sec || 0) + (this.msec || 0) / 1000;
+    return this.minus ? -res : res;
+  }
+  toString() {
+    return (this.minus ? "-" : "") + (this.hour >= 100 ? this.hour : fix0(this.hour || 0, 2)) + ":" + fix0(this.min || 0, 2) + (this.sec == undefined && this.msec == undefined ? "" : ":" + fix0(this.sec, 2)) + (this.msec == undefined ? "" : "." + fix0(this.msec, 3));
+  }
+  minAfter(min) {
+    return new Time(this.toSeconds() + min * 60);
+  }
+  minBefore(min) {
+    return new Time(this.toSeconds() - min * 60);
+  }
+  toTime(time) {
+    return time instanceof Time ? time : new Time(time);
+  }
+  add(time) {
+    time = this.toTime(time);
+    return new Time(this.toSeconds() + time.toSeconds());
+  }
+  sub(time) {
+    time = this.toTime(time);
+    return new Time(this.toSeconds() - time.toSeconds());
+  }
+  mul(time) {
+    time = this.toTime(time);
+    return new Time(this.toSeconds() * time.toSeconds());
+  }
+  div(time) {
+    time = this.toTime(time);
+    return new Time(this.toSeconds() / time.toSeconds());
+  }
+  mod(time) {
+    time = this.toTime(time);
+    return new Time(this.toSeconds() % time.toSeconds());
+  }
+  equals(time) {
+    time = this.toTime(time);
+    return this.toSeconds() == time.toSeconds();
+  }
+  contains(starttime, endtime) {
+    starttime = this.toTime(starttime);
+    endtime = this.toTime(endtime);
+    const t = this.toSeconds();
+    return t >= starttime.toSeconds() && t <= endtime.toSeconds();
+  }
+}
+
+export { Time };
---
+++ test/Time.test.js
@@ -0,0 +1,68 @@
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import { Time } from "../Time.js";
+
+Deno.test("time", () => {
+  t.assertEquals(new Time("07:09").toString(), "07:09");
+  t.assertEquals(new Time("07:09:59").toString(), "07:09:59");
+  t.assertEquals(new Time("07:09:59.999").toString(), "07:09:59.999");
+  t.assertEquals(new Time("-07:09").toString(), "-07:09");
+});
+Deno.test("toSeconds", () => {
+  t.assertEquals(new Time("00:00:10").toSeconds(), 10);
+  t.assertEquals(new Time("01:00").toSeconds(), 60 * 60);
+  t.assertEquals(new Time("01:00:10.123").toSeconds(), 60 * 60 + 10.123);
+  t.assertEquals(new Time("-01:00").toSeconds(), -60 * 60);
+});
+Deno.test("from sec", () => {
+  t.assertEquals(new Time(10).toString(), "00:00:10");
+  t.assertEquals(new Time(10.9999).toString(), "00:00:10.999");
+  t.assertEquals(new Time(60 * 60).toString(), "01:00:00");
+  t.assertEquals(new Time(100 * 60 * 60).toString(), "100:00:00");
+  t.assertEquals(new Time(-60 * 60).toString(), "-01:00:00");
+  t.assertEquals(new Time(-1).toString(), "-00:00:01");
+});
+Deno.test("minAfter", () => {
+  t.assertEquals(new Time("12:00").minAfter(30).toString(), "12:30:00");
+  t.assertEquals(new Time("12:00").minAfter(0).toString(), "12:00:00");
+  t.assertEquals(new Time("12:00").minAfter(60).toString(), "13:00:00");
+  t.assertEquals(new Time("12:00").minAfter(24 * 60).toString(), "36:00:00");
+});
+Deno.test("minBefore", () => {
+  t.assertEquals(new Time("12:00").minBefore(30).toString(), "11:30:00");
+  t.assertEquals(new Time("12:00").minBefore(0).toString(), "12:00:00");
+  t.assertEquals(new Time("12:00").minBefore(60).toString(), "11:00:00");
+  t.assertEquals(new Time("12:00").minBefore(24 * 60).toString(), "-12:00:00");
+});
+Deno.test("add", () => {
+  t.assertEquals(new Time("12:00").add(new Time(30)).toString(), "12:00:30");
+  t.assertEquals(new Time("12:00").add(30).toString(), "12:00:30");
+  t.assertEquals(new Time("12:00").add("00:00:30").toString(), "12:00:30");
+});
+Deno.test("sub", () => {
+  t.assertEquals(new Time("12:00").sub(new Time(30)).toString(), "11:59:30");
+  t.assertEquals(new Time("12:00").sub(30).toString(), "11:59:30");
+  t.assertEquals(new Time("12:00").sub("00:00:30").toString(), "11:59:30");
+});
+Deno.test("mul", () => {
+  t.assertEquals(new Time("12:00").mul(2).toString(), "24:00:00");
+  t.assertEquals(new Time("00:00:30").mul(30).toString(), "00:15:00");
+  t.assertEquals(new Time("00:00:30").mul(0).toString(), "00:00:00");
+});
+Deno.test("div", () => {
+  t.assertEquals(new Time("12:00").div(3).toString(), "04:00:00");
+  t.assertEquals(new Time("10:00:00").div(10).toString(), "01:00:00");
+  t.assertEquals(new Time("12:00").div("03:00").toSeconds(), 4);
+});
+Deno.test("mod", () => {
+  t.assertEquals(new Time("00:10:00").mod("00:03:00").toString(), "00:01:00");
+  t.assertEquals(new Time("00:10:00").mod(17).toString(), "00:00:05");
+});
+Deno.test("equals", () => {
+  t.assertEquals(new Time("01:00").equals(60 * 60), true);
+  t.assertEquals(new Time("00:00:30").equals(30), true);
+  t.assertEquals(new Time("00:00:30").equals(31), false);
+});
+Deno.test("contains", () => {
+  t.assertEquals(new Time("01:00").contains("01:00", "02:00"), true);
+  t.assertEquals(new Time("00:59").contains("01:00", "02:00"), false);
+});
