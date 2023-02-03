eee15151a2c3ef8b68e105df0f1181bed12c2ca8
Taisuke Fukuno <fukuno@jig.jp>
2023-02-03 09:24:14 +0900

    add getNextHoliday / getLastHoliday

--- Holiday.js
+++ Holiday.js
@@ -1,5 +1,7 @@
-import { HOLIDAY_JP } from "./HOLIDAY_JP.js";
 import { Day } from "./Day.js";
+//import { HOLIDAY_JP } from "./HOLIDAY_JP.js";
+const url = "https://code4fukui.github.io/day-es/holiday_jp.json";
+const HOLIDAY_JP = await (await fetch(url)).json();
 
 const toDay = (day) => {
   if (!day) {
@@ -25,5 +27,19 @@ const getHolidays = () => {
 const searchHoliday = (title) => {
   return HOLIDAY_JP.filter((h) => h.title == title).map((h) => h.date);
 };
+const getNextHoliday = (startday) => {
+  if (!startday) {
+    startday = new Day();
+  } else {
+    startday = toDay(startday);
+  }
+  return HOLIDAY_JP.find((h) => {
+    const h1 = new Day(h.date);
+    return h1.isAfter(startday);
+  });
+};
+const getLastHoliday = () => {
+  return HOLIDAY_JP[HOLIDAY_JP.length - 1];
+};
 
-export { getHoliday, getHolidays, isHoliday, searchHoliday };
+export { getHoliday, getHolidays, isHoliday, searchHoliday, getNextHoliday, getLastHoliday };
--- test/Holiday.test.js
+++ test/Holiday.test.js
@@ -1,5 +1,6 @@
 import * as t from "https://deno.land/std/testing/asserts.ts";
-import { getHoliday, isHoliday, searchHoliday } from "../Holiday.js";
+import { getHoliday, isHoliday, searchHoliday, getNextHoliday, getLastHoliday } from "../Holiday.js";
+import { Day } from "../Day.js";
 
 Deno.test("test", () => {
   t.assert(isHoliday("2021-01-01"));
@@ -15,9 +16,18 @@ Deno.test("search", () => {
     "2021-07-23",
     "2022-10-10",
     "2023-10-09",
+    "2024-10-14",
   ];
   t.assertEquals(searchHoliday("スポーツの日"), chk);
 });
 Deno.test("use Day.js", () => {
   t.assert(isHoliday("令和2年7月23日"));
+});
+Deno.test("getNextHoliday", () => {
+  t.assertEquals(getNextHoliday("2022-12-31"), { date: "2023-01-01", title: "元日" });
+  t.assertEquals(getNextHoliday("2023-01-01"), { date: "2023-01-02", title: "休日" });
+  t.assertEquals(getNextHoliday(), getNextHoliday(new Day()));
+});
+Deno.test("getLastHoliday", () => {
+  t.assertEquals(getLastHoliday(), { date: "2024-11-23", title: "勤労感謝の日" });
 });
