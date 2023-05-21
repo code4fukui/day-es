391d1044e014eecadbe378f3bee5130b697e8ad6
Taisuke Fukuno <fukuno@jig.jp>
2023-05-22 02:39:16 +0900

    fix typo

--- WAREKI.js
+++ WAREKI.js
@@ -1,6 +1,6 @@
 // https://github.com/code4fukui/BaseRegistry/blob/main/%E5%85%83%E5%8F%B7%E8%A5%BF%E6%9A%A6%E5%AF%BE%E5%BF%9C%E8%A1%A8.csv
 const WAREKI_ID = "MTSHR";
-const WAREKI_ID_JA = ["明示", "大正", "昭和", "平成", "令和"];
+const WAREKI_ID_JA = ["明治", "大正", "昭和", "平成", "令和"];
 const WAREKI_FIRST_YEAR = [1873, 1912, 1926, 1989, 2019];
 
 const WAREKI = {
